8dfab79a252e9585613294e7e32ab0f786a569e4
Quyk Mendonça <osilmar@outlook.com>
2018-07-21 16:41:16 -0300

    Create an ADO.NET Stored Procedure Wrapper

---
+++ Flogging/Flogging.Data/CustomADO/Sproc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Flogging.Data.CustomADO
+{
+    public class Sproc
+    {
+        private SqlCommand Command { get; set; }
+
+        public Sproc(SqlConnection connection, string procName, int timeoutSeconds = 30)
+        {
+            Command = new SqlCommand(procName, connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            if (timeoutSeconds != 30)
+            {
+                Command.CommandTimeout = timeoutSeconds;
+            }
+        }
+
+        public void SetParam(string paramName, object value)
+        {
+            Command.Parameters.Add(new SqlParameter(paramName, value ?? DBNull.Value));
+        }
+
+        public int ExecNonQuery()
+        {
+            try
+            {
+                return Command.ExecuteNonQuery(); //return nymber of rows affected
+            }
+            catch (Exception exception)
+            {
+                throw CreateProcedureException(exception);
+            }
+        }
+
+        private Exception CreateProcedureException(Exception exception)
+        {
+            var newException = new Exception("Stored Procedure call fail!", exception);
+            newException.Data.Add("Procedure:", Command.CommandText);
+            newException.Data.Add("ProcInputs:", GetInputString());
+            return newException;
+        }
+
+        private string GetInputString()
+        {
+            var inString = new StringBuilder();
+            foreach (SqlParameter commandParameter in Command.Parameters)
+            {
+                inString.Append($"{commandParameter.ParameterName} = {commandParameter.Value} |");
+            }
+
+            return inString.ToString();
+        }
+    }
+}
